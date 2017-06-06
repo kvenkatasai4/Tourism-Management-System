2083333d6ac9ab41577d8834ad3a5d5015b84847
nikesh2804 <nikesh2804@gmail.com>
2017-06-06 13:30:35 -0400

    Fist Submission

---
+++ TourismManagementSystem/src/connection/ConnectionString.java
@@ -0,0 +1,20 @@
+package connection;
+import java.sql.*;
+import static connection.Provider.*;
+
+public class ConnectionString {
+	static Connection con=null;
+	//single connection object for whole application since we used static
+	static{
+		try{
+			//we can use provider object properties because we statically imported Provider object
+			con=DriverManager.getConnection(CONNECTION_URL,USERNAME,PASSWORD);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+	}
+	//returns already existing connection object
+	public static Connection getCon(){
+		return con;
+	}
+}
---
+++ TourismManagementSystem/src/connection/Provider.java
@@ -0,0 +1,7 @@
+package connection;
+
+public interface Provider {
+	String CONNECTION_URL="jdbc:mysql://localhost/tourism";
+	String USERNAME="root";
+	String PASSWORD="";
+}
